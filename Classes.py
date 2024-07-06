45223f3556c250d0793aabf157d093f8b97d1a5e
Rayyan Al Mousa <rayyanalmousa92@gmail.com>
2024-07-06 16:25:22 +0300

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -14,27 +14,27 @@ class Graph:
             self.users[ID].Remove(ID)
     def removeRelationship(self,ID1,ID2):
         if ID1 in self.users and ID2 in self.users:
-            self.users[ID1].Remove(ID2)
-            self.users[ID2].Remove(ID1)
+            self.users[ID1].remove(ID2)
+            self.users[ID2].remove(ID1)
     def friends(self,ID):
         if ID in self.users:
-            self.users[ID].ListOfFriends()        
+            self.users[ID].listOfFriends()        
 class User:
-    def __init__(self,ID,Name,**interests):
+    def __init__(self,ID,name,**interests):
         self.ID=ID
-        self.Name=Name
-        self.Friends=set() 
+        self.name=name
+        self.friends=set() 
         for key ,value in interests.items():
             setattr(self,key,value)  
-    def Add(self,Friend):
-        self.Friends.add(Friend)
-    def Remove(self,Friend):
-        if Friend in self.Friends:
-            self.Friends.remove(Friend)
-    def ListOfFriends(self):
-        return self.Friends
+    def addFriend(self,friend):
+        self.friends.add(friend)
+    def removeFriend(self,friend):
+        if friend in self.friends:
+            self.friends.remove(friend)
+    def listOfFriends(self):
+        return self.friends
     def __str__(self):
-        return "ID",self.ID,"Name",self.Name,"Friends"
+        return "ID",self.ID,"Name",self.name,"Friends"
     def updateProfile(self,**interests):
         for key ,value in interests.items():
             setattr(self,key,value)
@@ -42,10 +42,16 @@ def main():
     user1=(123,"Rayyan")
     user2=(456,"Leen")
     user3=(789,"David")
+    user4=(124,"Leo")
     graph=Graph()
 #Add the users to the graph
     graph.addUser(user1)
     graph.addUser(user2)
     graph.addUser(user3)
+    graph.addUser(user4)
     graph.removeUser(user3)
+    User.addFriend(123,789)
+    User.addFriend(124,456)
+    User.removeFriend(123,789)
+
 main()
