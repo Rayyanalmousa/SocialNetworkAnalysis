1e03ebe9e8b5f53f54accd5393b3b3895d5b990e
Rayyan Al Mousa <rayyanalmousa92@gmail.com>
2024-07-07 22:08:39 +0300

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -31,19 +31,19 @@ class Graph:
             return self.users[ID].listOfFriends()
 
 
+    def __str__(self):
+        return "\n".join(str(user) for user in self.users.values())
+
 class User:
-    def __init__(self,ID,name,**interests):
+    def __init__(self,ID,name,interests=None):
         self.ID = ID
         self.name = name
         self.friends = set() 
-        for key ,value in interests.items():
-            setattr(self,key,value)  
-
+        self.interests = interests if interests else []
 
     def addFriend(self,friend):
         self.friends.add(friend)
         
-
 
     def removeFriend(self,friend):
         if friend in self.friends:
@@ -51,23 +51,23 @@ class User:
 
 
     def listOfFriends(self):
-        return self.friends
+        return list(self.friends)
     
-
-    def __str__(self):
-        return "ID: {}, Name: {}, Friends: {}".format(self.ID, self.name, self.friends)
-    
-
     def updateProfile(self,**interests):
         for key ,value in interests.items():
             setattr(self,key,value)
 
 
+    def __str__(self):
+        return "ID: {}, Name: {}, Friends: {}".format(self.ID, self.name, self.friends)
+    
+
+    
 def main():
-    user1 = (123,"Rayyan") 
-    user2 = (456,"Leen")
-    user3 = (789,"David")
-    user4 = (124,"Leo")
+    user1 = (123,"Rayyan","animals") 
+    user2 = (456,"Leen","books")
+    user3 = (789,"David","cars")
+    user4 = (124,"Leo","animals")
     graph = Graph()
 
 
@@ -76,9 +76,14 @@ def main():
     graph.addUser(user2)
     graph.addUser(user3)
     graph.addUser(user4)
+
     graph.removeUser(user3)
-    graph.addFriend(123,789)
-    graph.addFriend(124,456)
-    graph.removeFriend(123,789)
+
+    graph.addRelationship(123,789)
+    graph.addRelationship(124,456)
+
+    graph.removeRelationship(123,789)
+
     print(graph)
+    
 main() 
